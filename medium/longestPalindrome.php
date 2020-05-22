204a649c21cd0612a05cd495eb8d274ff4543096
陈兴生 <chenxingsheng@51shebao.com>
2020-05-22 14:54:33 +0800

    Queue

---
+++ medium/longestPalindrome.php
@@ -0,0 +1,15 @@
+<?php
+class Solution {
+
+    /**
+     * 题号：5
+     * @param String $s
+     * @return String
+     */
+    function longestPalindrome($s) {
+        $len = strlen($s);
+        for ($low=0; $low < $len-1; $low++) {
+
+        }
+    }
+}
